81cee787634fc07f44eb0080e3f28e14d0e73e8e
Richard Lyon <richardlyon@fastmail.com>
2020-01-06 18:45:25 +0000

    setup configuration data

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,34 @@
  * Solid blue LED - WiFi connected
  * Flashing red LED - MQTT server  connected, data transmitted
  * 
+ *  JSON V1.0
+ 
+    packetID = 1234
+    protocol n = 1.1
+    device
+      ID = ESP8266-001
+      type = ESP8266
+      location = livingroom
+      firmware n = 1.1 
+      os = mongoose
+      battery
+        active b = true
+        voltage n = 3.8
+      lora
+        RSSI n = -98
+        SNR n = 23
+        frequencyError n = 12234
+    sensors
+      SHT15
+        temperature n = 21.1
+        humidity n = 87
+      BH1750
+        lux n = 600
+      BMP388
+        mbars n = 1023
+    status 
+      message = OK
+      description = All's well
  * 
  */
 #include <string>
@@ -37,6 +65,7 @@ std::string temp_topic;
 std::string humidity_topic;
 std::string status_topic;
 std::string co2_topic;
+std::string message_topic;
 
 Sensor *sensor;
 bool hasCO2;
@@ -47,7 +76,7 @@ bool hasCO2;
  */
 static void timer_cb(void *)
 {
-  if (!sensor->isAvailable() || !mgos_sys_config_get_sensor_isActive()) return;
+  if (!sensor->isAvailable()) return;
 
   std::string temp = sensor->temperatureString();
   mgos_mqtt_pub(temp_topic.c_str(), temp.c_str(), temp.size(), 1, 0);
@@ -55,14 +84,10 @@ static void timer_cb(void *)
   std::string humidity = sensor->humidityString();
   mgos_mqtt_pub(humidity_topic.c_str(), humidity.c_str(), humidity.size(), 1, 0);
   
-  if (hasCO2)
-  {
-    std::string co2 = sensor->co2String();
-    mgos_mqtt_pub(co2_topic.c_str(), co2.c_str(), co2.size(), 1, 0);
-    LOG(LL_INFO, ("T: %s, H: %s, CO2: %s", temp.c_str(), humidity.c_str(), co2.c_str()));
-  } else {
-    LOG(LL_INFO, ("T: %s, H: %s", temp.c_str(), humidity.c_str()));
-  }
+  std::string co2 = sensor->co2String();
+  mgos_mqtt_pub(co2_topic.c_str(), co2.c_str(), co2.size(), 1, 0);
+
+  LOG(LL_INFO, ("T: %s, H: %s, CO2: %s", temp.c_str(), humidity.c_str(), co2.c_str()));
 
   mgos_mqtt_pub(status_topic.c_str(), "ONLINE", 6, 1, 0);
   mgos_gpio_blink(LED_RED, update_millis - 50, 50);
@@ -71,37 +96,21 @@ static void timer_cb(void *)
 enum mgos_app_init_result mgos_app_init(void)
 {
   // Create sensor from configuration
-  std::string sensor_name = std::string(mgos_sys_config_get_sensor_type());
-  if (sensor_name == "HDC1080")
-  {
-    sensor = mgos_HDC1080_create();
-    hasCO2 = mgos_sys_config_get_sensor_HDC1080_hasCO2();
-    LOG(LL_INFO, ("Starting sensor %s", sensor_name.c_str()));
-  }
-  else if (sensor_name == "SCD30")
-  {
-    int refresh_secs = mgos_sys_config_get_sensor_SCD30_refreshIntervalSecs();
-    int pressure_mbars = mgos_sys_config_get_sensor_SCD30_defaultAmbientPressureMbar();
-    hasCO2 = mgos_sys_config_get_sensor_SCD30_hasCO2();
-    sensor = mgos_SCD30_create(refresh_secs, pressure_mbars);
-    LOG(LL_INFO, ("Starting sensor %s, refresh %i secs, pressure %i mbars", sensor_name.c_str(), refresh_secs, pressure_mbars));
-  }
-  else
-  {
-    LOG(LL_ERROR, ("Couldn't initialise sensor with name %s, check configuration.", sensor_name.c_str()));
-    return MGOS_APP_INIT_ERROR;
-  }
-
+  // sensor = mgos_HDC1080_create();
+  int refresh_secs = mgos_sys_config_get_sensor_scd30_refreshIntervalSecs();
+  int pressure_mbars = mgos_sys_config_get_sensor_scd30_defaultAmbientPressureMbar();
+  sensor = mgos_SCD30_create(refresh_secs, pressure_mbars);
+  
   // Set up the red onboard LED to signal MQTT transfer
   mgos_gpio_set_mode(LED_RED, MGOS_GPIO_MODE_OUTPUT);
   mgos_gpio_write(LED_RED, true); // true is off for onboard LED
 
   // Build the MQTT topic strings
-  topic_root = std::string(mgos_sys_config_get_sensor_location()) + "/sensors/" + sensor_name;
-  temp_topic = topic_root + "/temp";
-  humidity_topic = topic_root + "/humidity";
-  status_topic = topic_root + "/status";
-  co2_topic = topic_root + "/co2";
+  topic_root = std::string(mgos_sys_config_get_device_location());
+  temp_topic = topic_root + "/sensors/scd30/temp";
+  humidity_topic = topic_root + "/sensors/scd30/humidity";
+  co2_topic = topic_root + "/sensors/scd30/co2";
+  message_topic = topic_root + "/status/message";
 
   // Set the MQTT will topic
   mgos_sys_config_set_mqtt_will_topic(status_topic.c_str());
