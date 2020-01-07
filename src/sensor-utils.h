842afb2ad0059dccbbd609df9ffc283e7256c5a7
Richard Lyon <richardlyon@fastmail.com>
2020-01-07 14:03:37 +0000

    new banner format

--- src/sensor-utils.h
+++ src/sensor-utils.h
@@ -11,23 +11,24 @@ namespace utils
  *      os
  *  json
  *      protocol_version    
- */     
+ */
 void printBanner()
 {
-    const char *firmware_title = mgos_sys_config_get_device_firmware_title();
+    const char *firmware_name = mgos_sys_config_get_device_firmware_name();
+    const char *device_location = mgos_sys_config_get_device_location();
+    const char *firmware_slug = mgos_sys_config_get_device_firmware_slug();
     const char *firmware_version = mgos_sys_config_get_device_firmware_version();
-    const char *device_os = mgos_sys_config_get_device_os();
-    const char *firmware_filename = mgos_sys_config_get_device_firmware_filename();
-    const char *json_version = mgos_sys_config_get_json_protocol_version();
+    const char *firmware_mcu = mgos_sys_config_get_device_type();
+    const char *firmware_os = mgos_sys_config_get_device_firmware_os();
     const char *device_id = mgos_sys_config_get_device_id(); // TODO FIXME get actual device id
 
     LOG(LL_INFO, ("======================================================================%d", NULL));
-    LOG(LL_INFO, ("# %s V%s", firmware_title, firmware_version));
+    LOG(LL_INFO, ("# %s (%s)", firmware_name, device_location));
     LOG(LL_INFO, ("----------------------------------------------------------------------%d", NULL));
-    LOG(LL_INFO, ("Firmware filename : %s", firmware_filename));
-    LOG(LL_INFO, ("Operating system  : %s", device_os));
-    LOG(LL_INFO, ("Protocol version  : %s", json_version));
-    LOG(LL_INFO, ("Device ID         : %s", device_id));
+    LOG(LL_INFO, ("Firmware  : %s v%s", firmware_slug, firmware_version));
+    LOG(LL_INFO, ("MCU       : %s", firmware_mcu));
+    LOG(LL_INFO, ("OS        : %s", firmware_os));
+    LOG(LL_INFO, ("Device ID : %s", device_id));
     LOG(LL_INFO, ("=======================================================================\n\n%d", NULL));
 }
 
